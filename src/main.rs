8a5c29338d53c16ee9dae547ab62e4e71a3f5021
ifTNT <happyjohn369@gmail.com>
2020-05-06 15:05:47 +0800

    Simple single-directional relay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![crate_name = "cafemq"]
+
 fn main() {
-    println!("Hello, world!");
+    let context = zmq::Context::new();
+    let rx = context.socket(zmq::REP).unwrap();
+    let tx = context.socket(zmq::REQ).unwrap();
+
+    assert!(rx.bind("tcp://*:4000").is_ok());
+    assert!(tx.connect("tcp://localhost:2000").is_ok());
+
+    loop {
+        let mut msg;
+        msg= rx.recv_multipart(0).unwrap();
+        println!("RX");
+        for i in &msg {
+            //println!("{}", hex::encode(i));
+            tx.send(i, 0).unwrap();
+        }
+        
+        msg = tx.recv_multipart(0).unwrap();
+        println!("TX");
+        for i in &msg {
+            //println!("{}", hex::encode(i));
+            rx.send(i, 0).unwrap();
+        }
+    }
 }
